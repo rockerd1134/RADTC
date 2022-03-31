694920bc60ae5db74b740dbdf78259c5b18f30d8
Rocker D'Antonio <rocker@juniper.net>
2022-03-30 23:04:39 -0700

    some grid, edge, node working with expand

--- src/lib/radtc/grid.py
+++ src/lib/radtc/grid.py
@@ -24,42 +24,47 @@ class Edge:
         return f"{self.source} -( {self.cost} )-> {self.destination}"
 
     def __eq__( self, other ):
-        return self.source == other.source and self.desitnation == other.destination and self.cost == other.cost
+        return self.source == other.source and self.destination == other.destination and self.cost == other.cost
     
     def __hash__( self ):
         return str( self )
 
 class Edges:
-  def __init__(self, edges: list[Edge] ) -> None:
-    #we store an edge map which is dictionary keyed by Nodes, with all connectiong nodes 
-    self.edge_map = {}
-    self.count = len( edges )
-    self.farthest_source = None
+    def __init__(self, edges: list[Edge] ) -> None:
+        #we store an edge map which is dictionary keyed by Nodes, with all connectiong nodes 
+        self.edge_map = {}
+        self.count = len( edges )
+        self.farthest_source = None
 
-    for edge in edges:
-        print( edge )
-        if not edge.source in self.edge_map:
-            self.edge_map[ edge.source ] = { }
-        self.edge_map[ edge.source ][ edge.destination ] = edge
-        #set farthest edge
-        if not isinstance( self.farthest_source, GridLocation ):
-            self.farthest_source = edge.source
+        for edge in edges:
+            if not edge.source in self.edge_map:
+                self.edge_map[ edge.source ] = { }
+            self.edge_map[ edge.source ][ edge.destination ] = edge
+            #set farthest edge
+            if not isinstance( self.farthest_source, GridLocation ):
+                self.farthest_source = edge.source
 
-        if self.farthest_source.x <= edge.source.x and self.farthest_source.y <= edge.source.y:
-            self.farthest_source = edge.source
+            if self.farthest_source.x <= edge.source.x and self.farthest_source.y <= edge.source.y:
+                self.farthest_source = edge.source
 
-  def get_node_edges( self, node ):
-    edges = []
-    for edge in list( self.edge_map[ node ].keys() ):
-        edges.append( self.edge_map[ node ][ edge ] ) 
-    return edges
-
-  def get_edges( self ):
-    edges = []
-    for node in list( self.edge_map.keys() ):
+    def get_node_edges( self, node ):
+        edges = []
         for edge in list( self.edge_map[ node ].keys() ):
             edges.append( self.edge_map[ node ][ edge ] ) 
-    return edges
+        return edges
+
+    def get_edges( self ):
+        edges = []
+        for node in list( self.edge_map.keys() ):
+            for edge in list( self.edge_map[ node ].keys() ):
+                edges.append( self.edge_map[ node ][ edge ] ) 
+        return edges
+
+    def get_edge_between_locations( self, src_loc, dest_loc ) -> 'Edge':
+        if src_loc in self.edge_map:
+            if dest_loc in self.edge_map[ src_loc ]:
+                return self.edge_map[ src_loc ][ dest_loc ]
+        return None
 
 
 #containes the needed information for creating a grid
@@ -88,10 +93,10 @@ class Grid:
 #        return str( output )
 
     @classmethod
-    def get_height_and_width_from_max_and_ratio( cls, max: int, ratio: int ) -> tuple:
+    def get_height_and_width_from_max_and_ratio( cls, max: int, ratio: int ) -> dict:
         x = int( max / ( ratio + 1 ) )
         y = int ( ratio * x  )
-        return ( x, y )
+        return { 'x': x, 'y':y, 'node_count': x * y, 'edge_count': x * y - x - y }
 
     @classmethod
     def _get_edge_cost_on_create( cls ):
@@ -111,12 +116,10 @@ class Grid:
             cardnality = int( config[ 'generate' ].get( 'cardnality', 0) )
 
             node_counts = Grid.get_height_and_width_from_max_and_ratio( max_nodes, hw_ratio )
-            x_max = node_counts[0] 
-            y_max = node_counts[1] 
+            x_max = node_counts[ 'x' ]
+            y_max = node_counts[ 'y' ]
 
             edges = []
-             
-            
             for x in range( x_max ):
                 for y in range( y_max ):
                     #make edge to the East
@@ -151,34 +154,50 @@ class Grid:
 
             return Grid( edges )
 
-                
-            #genorator has
-            #  max nodes
-            #  h/w ratio
-            #  edge range
-            #  max 
-            #  min
-            #  cardnality
-
-
-        return cls()
-
     @classmethod
-    def adjacent_north_of( cls, location: GridLocation ) -> GridLocation:
+    def adjacent_north_of( cls, location: 'GridLocation' ) -> 'GridLocation':
         return GridLocation( location.x, location.y + 1 )
 
     @classmethod
-    def adjacent_east_of( cls, location: GridLocation ) -> GridLocation:
+    def adjacent_east_of( cls, location: 'GridLocation' ) -> 'GridLocation':
         return GridLocation( location.x + 1, location.y )
 
     @classmethod
-    def adjacent_south_of( cls, location: GridLocation ) -> GridLocation:
+    def adjacent_south_of( cls, location: 'GridLocation' ) -> 'GridLocation':
         return GridLocation( location.x, location.y - 1 )
 
     @classmethod
-    def adjacent_west_of( cls, location: GridLocation ) -> GridLocation:
+    def adjacent_west_of( cls, location: 'GridLocation' ) -> 'GridLocation':
         return GridLocation( location.x - 1, location.y )
 
+    def location_is_in_bounds( self, loc: 'GridLocation' ) -> bool:
+        if loc.x >= self.GRID_X_START_INDEX and loc.y >= self.GRID_Y_START_INDEX:
+            #how to check max range?
+            if loc.x <= self.edges.farthest_source.x and loc.y <= self.edges.farthest_source.y:
+                return True
+        return False
+
+    def get_node( self, x: int, y: int ) -> 'Node':
+        loc = GridLocation( x, y )
+        if self.location_is_in_bounds( loc ):
+            return Node( self, loc )
+        else: 
+            return None
+
+    def get_node_at_location( self, loc: 'GridLocation' ) -> 'Node':
+        if self.location_is_in_bounds( loc ):
+            return Node( self, loc )
+        else: 
+            return None
+
+    def get_edge_between_locations( self, src_loc: 'GridLocation', dest_loc: 'GridLocation' ) -> 'Edge':
+        if self.location_is_in_bounds( src_loc ) and self.location_is_in_bounds( dest_loc ):
+            return self.edges.get_edge_between_locations( src_loc, dest_loc )
+        else:
+            return None
+
+    def get_edge_between_nodes( self, src: 'Node', dest: 'Node' ) -> 'Edge':
+        return self.get_edge_between_locations( src.location, dest.location )
 
 class Node:
     ''' A class that represents a node but is really an interface for 4 edges'''
@@ -186,31 +205,120 @@ class Node:
         self.location = grid_location
         self.grid = grid
 
-    def expand( self ):
+    def __str__( self ):
+        return str( self.location )
+
+    #### gets
+    def expand( self ) -> list[ 'Node' ]:
         ''' returns adjacent nodes N,E,W,S '''
+     
+        #TODO
+        #record the expantion
+
+        nodes = []
+        north = self.get_adjacent_north()
+        if north:
+            nodes.append( north )
+        east = self.get_adjacent_east()
+        if east:
+            nodes.append( east )
+        south = self.get_adjacent_south()
+        if south:
+            nodes.append( south )
+        west = self.get_adjacent_west()
+        if west:
+            nodes.append( west )
+        return nodes
+
+
+        
         pass
 
+    def get_adjacent_north( self ) -> 'Node':
+        return self.grid.get_node_at_location( self.grid.adjacent_north_of( self.location ) )
+
+    def get_adjacent_east( self ) -> 'Node':
+        return self.grid.get_node_at_location( self.grid.adjacent_east_of( self.location ) )
+
+    def get_adjacent_south( self ) -> 'Node':
+        return self.grid.get_node_at_location( self.grid.adjacent_south_of( self.location ) )
+
+    def get_adjacent_west( self ) -> 'Node':
+        return self.grid.get_node_at_location( self.grid.adjacent_west_of( self.location ) )
+
+
+    def get_edge_to( self, node: 'Node' ) -> 'Edge':
+        if self.is_adjacent( node ):
+            return self.grid.get_edge_between_nodes( self, node )
+        else:
+            return False
+
+    #### tests
     def is_adjacent( self, node: 'Node' ) -> bool:
         ''' checks if node is reachable '''
-        pass
+        if ( self.is_adjacent_east_of( node ) 
+            or self.is_adjacent_west_of( node ) 
+            or self.is_adjacent_north_of( node ) 
+            or self.is_adjacent_south_of( node ) ):
+            return True
+        else:
+            return False
 
     def is_north_of( self, node: 'Node' ) -> bool:
         ''' checks if node is north of a node'''
-        pass
+        if node.location.y < self.location.y: 
+            return True
+        else: 
+            return False
 
     def is_west_of( self, node: 'Node' ) -> bool:
         ''' checks if node is west of a node'''
-        pass
+        if node.location.x > self.location.x: 
+            return True
+        else: 
+            return False
 
     def is_south_of( self, node: 'Node' ) -> bool:
         ''' checks if node is south of a node'''
-        pass
+        if node.location.y > self.location.y: 
+            return True
+        else: 
+            return False
 
     def is_east_of( self, node: 'Node' ) -> bool:
         ''' checks if node is east of a node'''
-        pass
+        if node.location.x < self.location.x: 
+            return True
+        else: 
+            return False
 
+    def is_adjacent_north_of( self, node: 'Node' ) -> bool:
+        ''' checks if node is north of a node'''
+        if node.location.y + 1 == self.location.y: 
+            return True
+        else: 
+            return False
 
+    def is_adjacent_west_of( self, node: 'Node' ) -> bool:
+        ''' checks if node is west of a node'''
+        if node.location.x - 1 == self.location.x: 
+            return True
+        else: 
+            return False
+
+    def is_adjacent_south_of( self, node: 'Node' ) -> bool:
+        ''' checks if node is south of a node'''
+        if node.location.y - 1 == self.location.y: 
+            return True
+        else: 
+            return False
+
+    def is_adjacent_east_of( self, node: 'Node' ) -> bool:
+        ''' checks if node is east of a node'''
+        if node.location.x + 1 ==  self.location.x: 
+            return True
+        else: 
+            return False
 
 
 #class Lines:
--- src/lib/radtc/test/test_grid.py
+++ src/lib/radtc/test/test_grid.py
@@ -1,3 +1,5 @@
+from math import fabs
+from xml.dom.expatbuilder import FragmentBuilder
 import pytest 
 import sys
 sys.path.append( '../')
@@ -5,11 +7,11 @@ import grid
 
 def test_hw_ratio():
     res = grid.Grid.get_height_and_width_from_max_and_ratio( 100, 4 ) 
-    assert res[0] == 20
-    assert res[1] == 80
+    assert res[ 'x' ] == 20
+    assert res[ 'y' ] == 80
 
 def test_grid_created_from_config():
-    max_nodes = 10
+    max_nodes = 16
     hw_ratio = 1
     config = {
         'generate' :{
@@ -24,10 +26,93 @@ def test_grid_created_from_config():
 
     my_grid = grid.Grid.from_config( config )
     print( f"edge_count: {my_grid.edges.count}" ) 
-    print( f"farthest_source: {my_grid.edges.farthest_source}")
     my_edges = my_grid.edges.get_edges()
-    for edge in my_edges:
-        print( f"{str(edge.source)}" )
+    #for edge in my_edges:
+        ###print( f"{str(edge.source)} " )
+       #print( f"{str(edge)} " )
+
     
     assert isinstance( my_grid, grid.Grid )
-    #assert len( my_grid.edges == 20 * 2 + 18 * )
+    #assert 
+    
+
+def test_get_nodes():
+    max_nodes = 10
+    hw_ratio = 1
+    config = {
+        'generate' :{
+            'max_nodes' : max_nodes,
+            'hw_ratio' : hw_ratio,
+            #'edge_range' : 100,
+            #'edge_minimum' : 100,
+            #'impassible_percentage' : 100,
+            #'cardnality' : 100
+        }
+    }
+
+    my_grid = grid.Grid.from_config( config )
+    #print( f"farthest_source: {my_grid.edges.farthest_source}")
+    my_closest_node = my_grid.get_node( 0, 0 )
+    #print( f"node: {my_closest_node}" )
+    my_next_closest_east_node = my_grid.get_node( 1, 0 )
+    #print( f"east_node: {my_next_closest_east_node}" )
+    assert my_closest_node.is_west_of( my_next_closest_east_node )
+    assert not my_closest_node.is_east_of( my_next_closest_east_node )
+    assert not my_closest_node.is_south_of( my_next_closest_east_node )
+    assert not my_closest_node.is_north_of( my_next_closest_east_node )
+
+    my_next_closest_north_node = my_grid.get_node( 0, 1 )
+    #print( f"north_node: {my_next_closest_north_node}" )
+    assert not my_closest_node.is_west_of( my_next_closest_north_node )
+    assert not my_closest_node.is_north_of( my_next_closest_north_node )
+    assert my_closest_node.is_south_of( my_next_closest_north_node )
+    assert not my_closest_node.is_north_of( my_next_closest_north_node )
+
+    #test getting edge between two nodes
+    #print( f"node: {my_closest_node}" )
+    edge_to_north = my_closest_node.get_edge_to( my_next_closest_north_node )
+    edge_to_north_two = my_closest_node.get_edge_to( my_next_closest_north_node )
+    edge_from_north = my_next_closest_north_node.get_edge_to( my_closest_node )
+    #print( f"{str(edge_to_north)}" )
+    #print( f"{str(edge_from_north )}" )
+    assert edge_to_north != edge_from_north
+    assert edge_to_north == edge_to_north_two
+    edge_to_east = my_closest_node.get_edge_to( my_next_closest_east_node )
+
+    #my_farthest_node = my_grid.get_node( my_grid.edges.farthest_source.x, my_grid.edges.farthest_source.y )
+
+def test_get_expand():
+    max_nodes = 10
+    hw_ratio = 1
+    config = {
+        'generate' :{
+            'max_nodes' : max_nodes,
+            'hw_ratio' : hw_ratio,
+            #'edge_range' : 100,
+            #'edge_minimum' : 100,
+            #'impassible_percentage' : 100,
+            #'cardnality' : 100
+        }
+    }
+
+    #first test the first node. Should get two nodes
+    my_grid = grid.Grid.from_config( config )
+    my_closest_node = my_grid.get_node( 0, 0 )
+    print( f"node: {my_closest_node}" )
+    next_doors = my_closest_node.expand()
+    for node in next_doors:
+      print( node )
+
+    my_interesting_node = my_grid.get_node( 3, 2 )
+    print( f"node: {my_interesting_node}" )
+    next_doors = my_interesting_node.expand()
+    for node in next_doors:
+      print( node )
+
+    farthest_source = my_grid.edges.farthest_source
+    print( f"farthest_source: {farthest_source}" )
+    my_farthest_node = my_grid.get_node( farthest_source.x, farthest_source.y )
+    print( f"node: {my_farthest_node}" )
+    next_doors = my_farthest_node.expand()
+    for node in next_doors:
+      print( node )
